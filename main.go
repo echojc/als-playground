6017ab9fa318299090085fae2e71ab33174f3b09
Jonathan Chow <jonathan@movio.co>
2018-02-05 15:20:16 +1300

    --wip-- [skip ci]

--- main.go
+++ main.go
@@ -14,57 +14,57 @@ func init() {
 }
 
 func main() {
-	m, n := 10000, 1000
-	r := make([][]float64, m)
-	for i := range r {
-		r[i] = make([]float64, n)
-		for j := range r[i] {
-			if rand.Int()%2 == 0 {
-				r[i][j] = float64(rand.Intn(5) + 1)
-			}
-		}
-		if i%10000 == 0 {
-			fmt.Println(i)
-		}
-	}
-	//r := [][]float64{
-	//	[]float64{5, 3, 0, 1},
-	//	[]float64{4, 0, 0, 1},
-	//	[]float64{1, 1, 0, 5},
-	//	[]float64{1, 0, 0, 4},
-	//	[]float64{0, 1, 5, 4},
+	//m, n := 5, 4
+	//r := make([][]float64, m)
+	//for i := range r {
+	//	r[i] = make([]float64, n)
+	//	for j := range r[i] {
+	//		if rand.Int()%2 == 0 {
+	//			r[i][j] = float64(rand.Intn(5) + 1)
+	//		}
+	//	}
+	//	if i%10000 == 0 {
+	//		fmt.Println(i)
+	//	}
 	//}
+	r := [][]float64{
+		[]float64{1, 1, 0, 0},
+		[]float64{0, 1, 0, 0},
+		[]float64{1, 0, 1, 1},
+		[]float64{0, 1, 0, 1},
+		[]float64{0, 0, 1, 0},
+	}
 
 	// should be strictly less than min(columns, rows)
-	features := 10
+	features := 2
 	// regularization parameter, should be small-ish
-	lambda := 0.1
+	lambda := 0.01
 	// how many times to run the solver
-	iterations := 5
+	iterations := 10
 
 	start := time.Now()
-	//p, q := factorize(r, features, lambda, iterations)
-	factorize(r, features, lambda, iterations)
+	p, q := factorize(r, features, lambda, iterations)
+	//factorize(r, features, lambda, iterations)
 	end := time.Now()
 
 	// the original values are copied from here:
 	// http://www.quuxlabs.com/blog/2010/09/matrix-factorization-a-simple-tutorial-and-implementation-in-python/#basic-ideas
-	//fmt.Println("original")
-	//fmt.Printf("% v\n", mat64.Formatted(toDense(r)))
+	fmt.Println("\noriginal")
+	fmt.Printf("% v\n", mat64.Formatted(toDense(r)))
 
 	// the recommendations are similar in value to the result from the same source:
 	// http://www.quuxlabs.com/blog/2010/09/matrix-factorization-a-simple-tutorial-and-implementation-in-python/#implementation-in-python
-	//fmt.Println("\nrecommendations")
-	//out := mat64.NewDense(len(p[0]), len(q[0]), nil)
-	//out.Mul(toDense(p).T(), toDense(q))
-	//fmt.Printf("% .2f\n", mat64.Formatted(out, mat64.Squeeze()))
+	fmt.Println("\nrecommendations")
+	out := mat64.NewDense(len(p[0]), len(q[0]), nil)
+	out.Mul(toDense(p).T(), toDense(q))
+	fmt.Printf("% .2f\n", mat64.Formatted(out, mat64.Squeeze()))
 
 	// the feature matrices can be saved to recalculate recommendations
 	// but i think the recommendations matrix takes less space overall, so not sure if necessary
-	//fmt.Println("\nrow features")
-	//fmt.Println(mat64.Formatted(toDense(p).T()))
-	//fmt.Println("\ncolumn features")
-	//fmt.Println(mat64.Formatted(toDense(q).T()))
+	fmt.Println("\nrow features")
+	fmt.Println(mat64.Formatted(toDense(p).T()))
+	fmt.Println("\ncolumn features")
+	fmt.Println(mat64.Formatted(toDense(q).T()))
 
 	// time
 	fmt.Printf("\ntime: %dms\n", end.Sub(start)/time.Millisecond)
@@ -76,7 +76,7 @@ func factorize(r [][]float64, k int, lambda float64, count int) (p, q [][]float6
 	for i := range q {
 		q[i] = make([]float64, len(r[0]))
 		for j := range q[i] {
-			q[i][j] = 2 //rand.Float64()
+			q[i][j] = rand.Float64()
 		}
 	}
 
@@ -101,15 +101,15 @@ func factorize(r [][]float64, k int, lambda float64, count int) (p, q [][]float6
 	fmt.Println("transposed")
 
 	for i := 0; i < count; i++ {
-		start := time.Now()
+		//start := time.Now()
 		solveP(p, q, r)
-		end := time.Now()
-		fmt.Printf("\ni: %d p time: %dms\n", i, end.Sub(start)/time.Millisecond)
+		//end := time.Now()
+		//fmt.Printf("\ni: %d p time: %dms", i, end.Sub(start)/time.Millisecond)
 
-		start = time.Now()
+		//start = time.Now()
 		solveQ(p, q, rt)
-		end = time.Now()
-		fmt.Printf("\ni: %d p time: %dms\n", i, end.Sub(start)/time.Millisecond)
+		//end = time.Now()
+		//fmt.Printf("\ni: %d p time: %dms", i, end.Sub(start)/time.Millisecond)
 	}
 
 	return
@@ -179,8 +179,8 @@ func solveQ(p, q, rt [][]float64) {
 	wg.Add(len(q[0]))
 	for qj := range q[0] {
 		go func(qj int) {
-			defer wg.Done()
 			solveQj(p, q, rt[qj], qj)
+			wg.Done()
 		}(qj)
 	}
 	wg.Wait()
